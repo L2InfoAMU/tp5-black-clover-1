096a666c057b350e8adb23ed3252c6a24f5f2860
zainiryou <zainir.youssouf@etu.univ-amu.fr>
2018-11-30 18:58:28 +0100

    Tache 4

---
+++ src/main/java/image/SparseRasterImage.java
@@ -0,0 +1,34 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SparseRasterImage extends RasterImage{
+    public Map<Point,Color> map;
+
+    public SparseRasterImage(Color color, int width, int height){
+        constructor(color, width, height);
+    }
+
+    public SparseRasterImage(Color[][] colors){
+        constructor(colors);
+    }
+
+    @Override
+    public void createRepresentation(){
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public void setPixelColor(Color color, int x, int y){
+        map.put(new Point(x, y), color);
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y){
+        return map.get(new Point(x, y));
+    }
+
+}
